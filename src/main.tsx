a1ddb784a96270f1ea5826434087dfce455fa180
Deepa-TKumar <deept2107@gmail.com>
2025-01-09 15:25:57 +0530

    commit changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import LoginPage from './components/LoginScreen.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <LoginPage />
   </StrictMode>,
 )
