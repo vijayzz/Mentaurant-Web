6ce6ee02dfc937c27f7724c927b9f4bb7d9abaf4
rajalakshmi <rajalakshmisambanthan@gmail.com>
2025-01-20 11:17:12 +0530

    added the availabesvg and occupiedsvg

---
+++ src/globals/appImages.ts
@@ -0,0 +1,5 @@
+export const appImages = {
+    tableAvailable: require('../assets/images/AvailableImg.svg'),
+    tableOccupied : require('../assets/images/occupiedImg.svg')
+
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 
 
 import CarouselComponent from './components/MenuComponents/Carosal.tsx'
+import App from './App.tsx'
+import TableMain from './components/Tables/TableMain.tsx'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App /> */}
-    <CarouselComponent />
+     {/* <App /> */}
+    {/* <CarouselComponent /> */}
+    {/* <FloorTab/> */}
+    <TableMain/>
   </StrictMode>,
 )
