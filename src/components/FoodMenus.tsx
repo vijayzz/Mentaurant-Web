0db4fcc913d7f2b47dae01bfd8c3e8fc824b6a48
Mohana Priya <mohanabharani480@gmail.com>
2025-01-21 10:50:17 +0530

    updated my branch

--- src/components/FoodMenus.tsx
+++ src/components/FoodMenus.tsx
@@ -1,12 +1,12 @@
 
+
 import Foods from "./FoodCards/food";
 import MenuItems from "./MenuComponents/MenuItems";
-
-
 export default function FoodMenu(){
 
     return(
         <>
+        
         <MenuItems />
         <Foods />
         </>
